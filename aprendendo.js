eaed9f2979ae31f9aa83b3fbe6d7f0f2dacbece0
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-02 17:37:04 -0300

    Salvando um usuario no banco de dados

--- aprendendo.js
+++ aprendendo.js
@@ -4,9 +4,51 @@ const mongoose = require('mongoose')
 // Configuração do mongoDB
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/banco-teste").then( () => {
+mongoose.connect("mongodb://localost/banco-teste").then( () => {
     console.log("MongoDB conectado... ")
 }).catch( (err)=> {
     console.log("Houve um erro ao se conectar ao mongoDB:"+ err)
 })
 
+
+//  Model Usuários
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type:String,
+        require: true
+
+    },
+    sobrenome : {
+        type: String,
+        require: true
+    },
+    
+    email: {
+        type: String,
+        require: true
+    },
+
+    idade: {
+        type:Number,
+        require:true
+    },
+
+    pais: {
+        type:String
+    }
+})
+mongoose.model('usuarios', UsuarioSchema )
+
+const novoUsuario = mongoose.model('usuarios')
+
+new novoUsuario({
+    nome: "Aldo Pereira",
+    sobrenome:"Pereira",
+    email:"aldopereira@gmail.com",
+    idade: 30,
+    pais: "Brasil"
+}).save().then(() => {
+    console.log("Cliente salvo com sucesso!!!")
+}).catch( (err) => {
+    console.log("houve um erro ao salvar um usuário!!!"+err)
+})
