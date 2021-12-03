b98ab4ed1fbd8746df1965d2c98fd0cbae43accf
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-03 00:03:45 -0300

    Adicionando arquivos estaticos, bootstrap

--- blogapp/app.js
+++ blogapp/app.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
+const path = require('path')
+// const path = require('path')
 // const mongoose = require('mongoose')
 
 // configurações
@@ -17,11 +19,18 @@ const admin = require('./routes/admin')
 
     // Mongoose
 
+
+    //public 
+//     app.use(express.static(path.join(__dirname, "public")))
+    // app.use(express.static(path.join( 'public/css')))
+    console.log("caminho+"+ __dirname);
+    app.use(express.static(path.join(__dirname, "public")))
+
 // Rotas 
     app.use('/admin', admin)
 
 const PORT = 8081
 
 app.listen(PORT, ()=> {
-    console.log('servidor rodando!!! ')
+    console.log('servidor rodando!!! no endereço: localhost:8081/admin'+PORT)
 })
