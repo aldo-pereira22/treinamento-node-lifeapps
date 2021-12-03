4acaec4e29f26a02a58d5fea4a91a696778c13d0
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-02 21:23:12 -0300

    Configurando as rotas

--- blogapp/app.js
+++ blogapp/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
+const admin = require('./routes/admin')
 // const mongoose = require('mongoose')
 
 // configurações
@@ -16,6 +17,8 @@ const app = express()
 
     // Mongoose
 
+// Rotas 
+    app.use('/admin', admin)
 
 const PORT = 8081
 
