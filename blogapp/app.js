020208985644c07a7464cc3944bab32b07a868f8
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2021-12-02 21:05:24 -0300

    Configurando handlebars-express

---
+++ blogapp/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+// const mongoose = require('mongoose')
+
+// configurações
+    //Body-Parser
+        app.use(bodyParser.urlencoded({extended:true}))
+        app.use(bodyParser.json())
+
+    // Handlebars
+        // app.engine('handlebars',handlebars({defaultLayout: 'main'}))
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+
+    // Mongoose
+
+
+const PORT = 8081
+
+app.listen(PORT, ()=> {
+    console.log('servidor rodando!!! ')
+})
